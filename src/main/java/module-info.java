e02f12df1a04c68a271cab5316445169893dca0e
Anjelo Esperanzate <2240084@slu.edu.ph>
2025-03-05 19:58:41 +0800

    Initial structure commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module Lendify {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.base;
+    requires java.logging;
+    requires java.xml;
+    requires java.desktop;
+
+}
