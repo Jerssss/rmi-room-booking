4ab1673fe1a121caaa0664fa5f6ad40b2479293c
Sebasthian Molina <2242541@slu.edu.ph>
2025-03-12 00:52:55 +0800

    Created the AddNewTerminalView along with the initialization of the buttons and actions with the connection and using RMI (without functionality yet)

--- src/main/java/client/admin/view/AddNewTerminalView.java
+++ src/main/java/client/admin/view/AddNewTerminalView.java
@@ -1,6 +1,64 @@
 package client.admin.view;
 
-//you can get the code in the integ clone repository
-// but edit it accordingly to our new structure
-public class AddNewTerminalView {
+import client.admin.controller.AddNewTerminalController;
+import client.admin.model.AddNewTerminalModel;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.BorderPane;
+import shared.Terminal;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AddNewTerminalView implements Initializable {
+
+    @FXML
+    private TableView<Terminal> addTerminalTableView;
+    @FXML
+    private TableColumn<Terminal, String> terminalColumn;
+    @FXML
+    private TableColumn<Terminal, String> roomNumberColumn;
+    @FXML
+    private TableColumn<Terminal, String> terminalOSColumn;
+    @FXML
+    private TableColumn<Terminal, String> startTimeColumn;
+    @FXML
+    private TableColumn<Terminal, String> endTimeColumn;
+    @FXML
+    private TableColumn<Terminal, String> statusColumn;
+
+    private AddNewTerminalController controller;
+    private BorderPane rootPane;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        initializeTableColumns();
+        System.out.println("[CLIENT] Table columns initialized successfully.");
+        this.controller = new AddNewTerminalController(this, new AddNewTerminalModel());
+        controller.loadTerminals();  // Fetch & update table on start
+    }
+
+
+    private void initializeTableColumns() {
+        terminalColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getTerminalID()));
+        roomNumberColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getRoom()));
+        terminalOSColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getOs()));
+        startTimeColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getStartTime()));
+        endTimeColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getEndTime()));
+        statusColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getStatus()));
+    }
+
+
+    /**
+     * Get TableView
+     */
+    public TableView<Terminal> getTableView() {
+        return addTerminalTableView;
+    }
+
+    public BorderPane getRootPane() {
+        return rootPane;
+    }
 }
