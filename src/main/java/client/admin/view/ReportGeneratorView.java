2ef33e0b452cd4d43ef8d11c1c2b8488480ca8f2
Bryant Casey Pangilinan <2153766@slu.edu.ph>
2025-03-12 20:07:48 +0800

    Implemented the ReportGeneratorView for displaying reports.

--- src/main/java/client/admin/view/ReportGeneratorView.java
+++ src/main/java/client/admin/view/ReportGeneratorView.java
@@ -1,6 +1,33 @@
 package client.admin.view;
 
-//you can get the code in the integ clone repository
-// but edit it accordingly to our new structure
+import client.admin.controller.ReportGeneratorController;
+import javafx.collections.ObservableList;
+import server.utility.LogReport;
+import server.utility.ReservationReport;
+
 public class ReportGeneratorView {
+    private final ReportGeneratorController controller;
+
+    public ReportGeneratorView(ReportGeneratorController controller) {
+        this.controller = controller;
+    }
+
+    public void displayActivityReport() {
+        controller.loadLogsData();
+        controller.loadReservationReports();
+    }
+
+    public void setLogsData(ObservableList<LogReport> logs) {
+        System.out.println("\n===== Log Reports =====");
+        for (LogReport log : logs) {
+            System.out.println(log);
+        }
+    }
+
+    public void setReservationReports(ObservableList<ReservationReport> reservations) {
+        System.out.println("\n===== Reservation Reports =====");
+        for (ReservationReport reservation : reservations) {
+            System.out.println(reservation);
+        }
+    }
 }
