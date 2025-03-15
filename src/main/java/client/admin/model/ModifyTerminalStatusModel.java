bd4137f2b0313d3e643907d7d832a24a569d4131
Giana <2241883@slu.edu.ph>
2025-03-15 23:10:38 +0800

    added a method to call the method that updates the terminal data from the server

--- src/main/java/client/admin/model/ModifyTerminalStatusModel.java
+++ src/main/java/client/admin/model/ModifyTerminalStatusModel.java
@@ -3,41 +3,32 @@ package client.admin.model;
 import client.ClientMain;
 import shared.Terminal;
 import shared.interfaces.admin.AdminProcessors;
-
-import java.rmi.RemoteException;
 import java.util.List;
 
 public class ModifyTerminalStatusModel {
-    private AdminProcessors adminProcessors;
+    private AdminProcessors adminService;
 
     public ModifyTerminalStatusModel() {
-        this.adminProcessors = ClientMain.getAdminProcessors(); // Get RMI instance
+        this.adminService = ClientMain.getAdminProcessors(); // Get RMI instance
     }
 
-    public List<Terminal> fetchReservations() {
-        System.out.println("[CLIENT] fetchReservations() method called.");
-
+    public List<Terminal> fetchTerminals() {
         try {
-            if (adminProcessors == null) {
-                System.err.println("[ERROR] AdminProcessors RMI service is NULL!");
-                return null;
-            }
-
-            List<Terminal> terminals = adminProcessors.getAllTerminals();
-
-            if (terminals == null || terminals.isEmpty()) {
-                System.out.println("[SERVER] No reservations found via RMI.");
-            } else {
-                System.out.println("[CLIENT] Loaded " + terminals.size() + " reservations via RMI.");
-                for (Terminal terminal : terminals) {
-                    System.out.println("[SERVER] " + terminal);
-                }
-            }
-
-            return terminals;
-        } catch (RemoteException e) {
-            System.err.println("[ERROR] RMI call failed: " + e.getMessage());
+            return adminService.getAllTerminals();
+        } catch (Exception e) {
+            System.err.println("[ERROR] Failed to fetch terminals: " + e.getMessage());
             return null;
+        }
+    }
+
+    public boolean saveModifiedTerminals(List<Terminal> terminals) {
+        try {
+            System.out.println("=====================================================");
+            System.out.println("[CLIENT] Modified terminal status have been saved");
+            return adminService.modifyTerminalStatus(terminals);
+        } catch (Exception e) {
+            System.err.println("[ERROR] Failed to modify terminal via RMI: " + e.getMessage());
+            return false;
         }
     }
 }
