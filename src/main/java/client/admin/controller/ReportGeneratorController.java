8beb741ddbd037348f765756f956ea8067350d17
Gab <alysongabrielflores@gmail.com>
2025-03-20 23:09:39 +0800

    restructured model for unity for the instantiation of the processors

--- src/main/java/client/admin/controller/ReportGeneratorController.java
+++ src/main/java/client/admin/controller/ReportGeneratorController.java
@@ -16,21 +16,4 @@ public class ReportGeneratorController {
         this.view = view;
     }
 
-//    public void loadLogsData() {
-//        ObservableList<LogReport> logs = model.loadLogsFromServer();
-//        view.setLogsData(logs);
-//    }
-
-//    public void loadReservationReports() {
-//        ObservableList<ReservationReport> reservations = model.loadReservationsFromServer();
-//        System.out.println("Reservations loaded into View: " + reservations.size());
-//        view.setReservationReports(reservations);
-//    }
-//
-//    public void searchReports(String query) {
-//        ObservableList<LogReport> logs = model.searchLogs(query);
-//        ObservableList<ReservationReport> reservations = model.searchReservations(query);
-//        view.setLogsData(logs);
-//        view.setReservationReports(reservations);
-//    }
 }
--- src/main/java/client/admin/model/ReportGeneratorModel.java
+++ src/main/java/client/admin/model/ReportGeneratorModel.java
@@ -7,15 +7,4 @@ import java.util.List;
 
 public class ReportGeneratorModel {
 
-    private static final File LOGS_FILE = new File("src/main/resources/data/logs.json");
-
-    public List<Log> generateActivityReport() {
-        try {
-            System.out.println("[ReportGeneratorModel] Generating activity report...");
-            return JSONUtility.loadLogs(LOGS_FILE);
-        } catch (Exception e) {
-            System.err.println("[ERROR] Failed to generate activity report: " + e.getMessage());
-            return null;
-        }
-    }
 }
