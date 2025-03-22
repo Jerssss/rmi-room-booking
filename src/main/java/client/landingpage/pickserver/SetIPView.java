948504bed2c713bcc855cbc8487268c65e37606e
Anjelo Esperanzate <2240084@slu.edu.ph>
2025-03-22 11:30:10 +0800

    Created the server ip picker and its view class. TODO: Controller and Model (if needed).

---
+++ src/main/java/client/landingpage/pickserver/SetIPView.java
@@ -0,0 +1,16 @@
+package client.landingpage.pickserver;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+public class SetIPView {
+
+    @FXML
+    public ComboBox serversComboBox;
+    @FXML
+    private Button connectButton;
+    @FXML
+    private TextField ipTextField;
+}
