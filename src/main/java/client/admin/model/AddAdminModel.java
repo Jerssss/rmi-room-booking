c51331ff02fbd7ab326a87b6ec374ceb31e25afe
Sebasthian Molina <2242541@slu.edu.ph>
2025-03-25 11:54:07 +0800

    Edited and Modified the AddAdminModel and AddAdminView to ensure working functionality of the AddAdmin, I fixed the create button not doing anything as stated by the previous committer

--- src/main/java/client/admin/model/AddAdminModel.java
+++ src/main/java/client/admin/model/AddAdminModel.java
@@ -1,24 +1,32 @@
 package client.admin.model;
 
 import client.ClientMain;
+import shared.Admin;
 import shared.interfaces.Authentication;
+import shared.interfaces.admin.AdminProcessors;
 
 import java.rmi.RemoteException;
 
 public class AddAdminModel {
     private final Authentication authService;
+    private final AdminProcessors adminService;
 
     public AddAdminModel() {
         this.authService = ClientMain.getAuthService();
+        this.adminService = ClientMain.getAdminService();
     }
 
-    public void registerAdmin(String adminID, String name, String password, String facultyType)
-            throws RemoteException {
-        if (authService == null) {
-            System.err.println("[ERROR] AuthService is NULL!"); // Debug
+    public void registerAdmin(String adminID, String name, String password, String facultyType) throws RemoteException {
+        AdminProcessors adminService = ClientMain.getAdminService();
+
+        if (adminService == null) {
+            System.err.println("[ERROR] AdminProcessorService is NULL! Check RMI connection.");
             throw new RemoteException("Server error");
         }
-        System.out.println("[DEBUG] Calling authService.signUp()..."); // Debug
-        authService.signUp(adminID, name, password, "Admin", "", facultyType);
+
+        System.out.println("[CLIENT] Sending new admin data to server...");
+        Admin newAdmin = new Admin(adminID, name, "Admin", password, facultyType);
+        adminService.registerAdmin(newAdmin);
+        System.out.println("[CLIENT] Admin successfully registered!");
     }
 }
